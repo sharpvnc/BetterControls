00a50b5d9d9597772cf034abaa3ca1f06d11d961
Jamie Highfield <jamie@jamiehighfield.co.uk>
2022-05-19 09:21:49 +0100

    Added helper class for specifying designer categories for properties.

---
+++ src/BetterControls/ComponentModel/Categories.cs
@@ -0,0 +1,9 @@
+﻿namespace BetterControls.ComponentModel
+{
+    internal static class Categories
+    {
+        public const string Appearance = "Appearance";
+        public const string Behavior = "Behavior";
+        public const string Data = "Data";
+    }
+}
