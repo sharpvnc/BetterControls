f9dd0144f8e07247da93512b360202ab564097aa
Jamie Highfield <jamie@jamiehighfield.co.uk>
2022-05-19 08:49:24 +0100

    Added classes to represent a toggle button.

---
+++ src/BetterControls/BetterToolbar/Items/BetterToolbarToggleButton.NativeStructures.cs
@@ -0,0 +1,34 @@
+﻿namespace BetterControls
+{
+    /// <summary>
+    /// Represents a toolbar item that is a toggle button.
+    /// </summary>
+    partial class BetterToolbarToggleButton
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns><inheritdoc/></returns>
+        internal override NativeMethods.TBBUTTON ComputeTbButton()
+        {
+            NativeMethods.TBBUTTON button = base.ComputeTbButton();
+
+            button.fsStyle = NativeMethods.TBSTYLE_CHECK | 0x0010;
+
+            return button;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns><inheritdoc/></returns>
+        internal override NativeMethods.TBBUTTONINFO ComputeTbButtonInfo()
+        {
+            NativeMethods.TBBUTTONINFO button = base.ComputeTbButtonInfo();
+
+            button.fsStyle = NativeMethods.TBSTYLE_CHECK | 0x0010;
+
+            return button;
+        }
+    }
+}
---
+++ src/BetterControls/BetterToolbar/Items/BetterToolbarToggleButton.cs
@@ -0,0 +1,57 @@
+﻿namespace BetterControls
+{
+    /// <summary>
+    /// Represents a toolbar item that is a toggle button.
+    /// </summary>
+    public partial class BetterToolbarToggleButton : BetterToolbarClickableButton
+    {
+        /// <summary>
+        /// Initialize a new instance of <see cref="BetterToolbarToggleButton"/>.
+        /// </summary>
+        public BetterToolbarToggleButton() { }
+
+        /// <summary>
+        /// Initialize a new instance of <see cref="BetterToolbarToggleButton"/>.
+        /// </summary>
+        /// <param name="text">The text of the button.</param>
+        public BetterToolbarToggleButton(string text)
+            : base(text)
+        { }
+
+        /// <summary>
+        /// Initialize a new instance of <see cref="BetterToolbarToggleButton"/>.
+        /// </summary>
+        /// <param name="text">The text of the button.</param>
+        /// <param name="description">The description of the button.</param>
+        public BetterToolbarToggleButton(string text, string description)
+            : base(text, description)
+        { }
+
+        /// <summary>
+        /// Initialize a new instance of <see cref="BetterToolbarToggleButton"/>.
+        /// </summary>
+        /// <param name="text">The text of the button.</param>
+        /// <param name="imageIndex">The index of the image from the toolbar image list to be shown in the button.</param>
+        public BetterToolbarToggleButton(string text, int imageIndex)
+            : base(text, imageIndex)
+        { }
+
+        /// <summary>
+        /// Initialize a new instance of <see cref="BetterToolbarToggleButton"/>.
+        /// </summary>
+        /// <param name="text">The text of the button.</param>
+        /// <param name="description">The description of the button.</param>
+        /// <param name="imageIndex">The index of the image from the toolbar image list to be shown in the button.</param>
+        public BetterToolbarToggleButton(string text, string description, int imageIndex)
+            : base(text, description, imageIndex)
+        { }
+
+        /// <summary>
+        /// Initialize a new instance of <see cref="BetterToolbarToggleButton"/>.
+        /// </summary>
+        /// <param name="ownerToolbar">The owner toolbar as an instance of <see cref="BetterToolbar"/>.</param>
+        private protected BetterToolbarToggleButton(BetterToolbar ownerToolbar)
+            : base(ownerToolbar)
+        { }        
+    }
+}
