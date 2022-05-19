11e44aadd575b52f28594b8f16043172c1136059
Jamie Highfield <jamie@jamiehighfield.co.uk>
2022-05-19 09:20:59 +0100

    Added base classes for all classes that can be contained within an ElementeCollection<T> collection.

---
+++ src/BetterControls/Element.cs
@@ -0,0 +1,35 @@
+﻿using BetterControls.ComponentModel;
+using System;
+using System.ComponentModel;
+
+namespace BetterControls
+{
+    /// <summary>
+    /// Extend this class to create an element.
+    /// </summary>
+    [Serializable]
+    public abstract class Element : IElement
+    {
+        /// <summary>
+        /// Initialize a new instance of <see cref="Element"/>.
+        /// </summary>
+        protected Element() { }
+
+        private object _tag;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [Category(Categories.Data)]
+        [Description("The object that contains data about the element.")]
+        [DefaultValue(null)]
+        [Localizable(false)]
+        [Bindable(true)]
+        [TypeConverter(typeof(StringConverter))]
+        public object Tag
+        {
+            get => _tag;
+            set => _tag = value;
+        }
+    }
+}
---
+++ src/BetterControls/IElement.cs
@@ -0,0 +1,13 @@
+﻿namespace BetterControls
+{
+    /// <summary>
+    /// Implement this interface to create an element.
+    /// </summary>
+    public interface IElement
+    {
+        /// <summary>
+        /// Gets or sets the object that contains data about the element.
+        /// </summary>
+        object Tag { get; set; }
+    }
+}
